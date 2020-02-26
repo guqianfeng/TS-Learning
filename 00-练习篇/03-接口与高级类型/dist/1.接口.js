a8da36f03740a8bb1de9fb7e85395663eec78a62
qianfengg <fred_gqf0221@163.com>
2020-02-26 14:37:43 +0800

    接口的学习

--- 00-练习篇/03-接口与高级类型/dist/1.接口.js
+++ 00-练习篇/03-接口与高级类型/dist/1.接口.js
@@ -4,3 +4,31 @@ let p1 = {
 };
 // p1.x = 20; //只读属性会报错
 p1.z = 30;
+// function fn1(a: number, b: number): number {
+//     return a + b;
+// }
+// function fn2(a: number, b: number): number {
+//     return a * b;
+// }
+let fn1 = function (a, b) {
+    return a + b;
+};
+let fn2 = function (a, b) {
+    return a * b;
+};
+function on(el, eventName, callback) {
+    el.addEventListener(eventName, callback);
+}
+let div = document.getElementById("test");
+if (div) {
+    on(div, "click", e => {
+    });
+}
+let box = {
+    width: 100,
+    height: 200,
+    scale: 5,
+    fn(a) {
+        return a;
+    }
+};
--- 00-练习篇/03-接口与高级类型/src/1.接口.ts
+++ 00-练习篇/03-接口与高级类型/src/1.接口.ts
@@ -18,3 +18,58 @@ interface PointTest {
     [props: string]: Object;
     [props: number]: Date;
 }
+
+interface IFunc {
+    (a: number, b: number): number;
+}
+
+// function fn1(a: number, b: number): number {
+//     return a + b;
+// }
+// function fn2(a: number, b: number): number {
+//     return a * b;
+// }
+
+let fn1: IFunc = function(a: number, b: number): number{
+    return a + b;
+}
+
+let fn2: IFunc = function(a: number, b: number): number{
+    return a * b;
+}
+
+interface IEvent {
+    (e: Event): void;
+    // (e: MouseEvent): void;
+}
+
+function on(el: Element, eventName: string, callback: IEvent){
+    el.addEventListener(eventName, callback);
+}
+
+let div = document.getElementById("test");
+if(div){
+    on(div, "click", e => {
+
+    })
+}
+
+interface Box {
+    width: number;
+    height: number;
+    fn(a: string): string;
+}
+interface Box {
+    scale: number;
+    // width: string; //会报错，因为同名的属性，类型要一致
+    fn(a: number): number; //重载
+}
+
+let box = {
+    width: 100,
+    height: 200,
+    scale: 5,
+    fn(a: any): any{
+        return a;
+    }
+}
